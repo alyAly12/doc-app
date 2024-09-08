5c6aac6e1caf23d3c74e8bf6684e8ea5a6f800d5
mohamed <medoaly870@gmail.com>
2024-09-08 19:08:28 +0300

    adding signup screen

---
+++ lib/features/signup_feature/presentation/screens/signup_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/signup_screen_body.dart';
+
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: const Scaffold(
+          backgroundColor: Colors.white,
+          body:SignupScreenBody()
+      ),
+    );
+  }
+}
--- lib/features/signup_feature/presentation/widgets/have_an_account_widget.dart
+++ lib/features/signup_feature/presentation/widgets/have_an_account_widget.dart
@@ -4,28 +4,28 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../core/routing/routes.dart';
 import '../../../../core/utils/app_colors.dart';
 
-class DontHaveAccountWidget extends StatelessWidget {
-  const DontHaveAccountWidget({super.key});
+class HaveAnAccountWidget extends StatelessWidget {
+  const HaveAnAccountWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        context.pushNamed(Routes.signupScreen);
+        context.pushNamed(Routes.loginScreen);
       },
       child: RichText(
           textAlign: TextAlign.center,
           text: TextSpan(
             children: [
               TextSpan(
-                text: 'Don\'t have an account yet? ',
+                text: 'Have an account yet? ',
                 style: TextStyle(
                   color: AppColors.greyColor,
                   fontSize: 15.sp,
                 ),
               ),
               TextSpan(
-                text: 'Sign up',
+                text: 'Login',
                 style: TextStyle(
                   color: AppColors.mainColor,
                   fontSize: 15.sp,
